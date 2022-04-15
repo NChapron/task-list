7d60105ea6c4779952344b0347fd6d9677dd84bb
Nicolas Chapron <nicolas.chapron2022@gmail.com>
2022-04-15 11:34:56 +0100

    Disable empty input

--- src/components/TaskList/TaskInput/TaskInput.js
+++ src/components/TaskList/TaskInput/TaskInput.js
@@ -12,6 +12,9 @@ const TaskInput = (props) => {
 
   const formSubmitHandler = (event) => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0) {
+      return;
+    }
     props.onAddTask(enteredValue);
   };
 
